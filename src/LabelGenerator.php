a3c7dfb70b02f1449da02cda5516cdb5ee67dc73
Dummdevka <87282400+Dummdevka@users.noreply.github.com>
2022-02-10 20:55:50 +0300

    Data validation

--- src/LabelGenerator.php
+++ src/LabelGenerator.php
@@ -8,6 +8,15 @@ class LabelGenerator
         if( !isset( $_GET['firstname'] ) || !isset( $_GET['lastname'] ) || !isset( $_GET['phone']) || strlen( $_GET['firstname'] )<1 || strlen($_GET['lastname'])<1 || strlen($_GET['phone'])<1 ) {
             return json_encode( 'Please fill in all the data' );
         }
+        if( !is_numeric( $_GET['phone']) || strlen( $_GET['phone'])>25 ) {
+            return json_encode( 'Invalid phone format' );
+        }
+        if( strlen( $_GET['firstname'])>25 ) {
+            return json_encode( 'Invalid firstname format' );
+        }
+        if( strlen( $_GET['lastname'])>25 ) {
+            return json_encode( 'Invalid lastname format' );
+        }
         return json_encode(
             [
                 'Name' => $_GET['firstname'] . ' ' . $_GET['lastname'],
